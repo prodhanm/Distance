bd7047ef98024b5bc3d6b5c8a9af7e9734cc4d26
prodhanm <mustafizur.prodhan@gmail.com>
2025-04-14 02:20:20 -0400

    The program successfully calculates distance

--- Distance.cpp
+++ Distance.cpp
@@ -2,7 +2,21 @@
 
 using namespace std;
 
-int main() {
-    return 0;
+int distanceV(float v0, float v, float a) {
+    float d = (pow(v, 2) - pow(v0, 2) / (2 * a));
+    return d;
+}
+
+void main() {
+    float v0, v, a;
+    cout << "Enter the inital velocity: ";
+    cin >> v0;
+    cout << "Enter the final velocity: ";
+    cin >> v;
+    cout << "Enter the acceleration: ";
+    cin >> a;
+    float d = distanceV(v0, v, a);
+    printf("The distance is %.5f meters", d);
+
 }
 
